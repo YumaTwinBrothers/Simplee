5b7ac37a050e1fd04fc474fb080fd2a9e6136c85
github YumaTwinBrothers <y_m_yolo@yahoo.co.jp>
2020-03-30 17:14:10 +0900

    Add image uploader

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_30_054356) do
+ActiveRecord::Schema.define(version: 2020_03_30_075843) do
 
   create_table "article_categories", force: :cascade do |t|
     t.integer "article_id"
@@ -38,6 +38,7 @@ ActiveRecord::Schema.define(version: 2020_03_30_054356) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.boolean "admin", default: false
+    t.string "image"
   end
 
 end
